b961d410f0a4482c55e1d302e89c71d1e5ff5628
KillDare <87498894+KillDare@users.noreply.github.com>
2026-02-08 20:03:19 -0300

    Deploy web

--- utils/alert.ts
+++
@@ -1,9 +0,0 @@
-import { Platform, Alert as RNAlert } from 'react-native';
-
-export function showAlert(title: string, message: string) {
-  if (Platform.OS === 'web') {
-    window.alert(`${title}\n\n${message}`);
-  } else {
-    RNAlert.alert(title, message);
-  }
-}
